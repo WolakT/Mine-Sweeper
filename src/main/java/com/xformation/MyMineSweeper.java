e9f67482f74376e83d90ba6e974c05119c013bcc
Tomcio <tomasz.wolak@pl.ibm.com>
2017-09-06 23:47:11 +0200

    "second interface method implemented"

--- src/main/java/com/xformation/MyMineSweeper.java
+++ src/main/java/com/xformation/MyMineSweeper.java
@@ -4,45 +4,59 @@ package com.xformation;
  * Created by WolakT on 2017-09-05.
  */
 public class MyMineSweeper implements MineSweeper {
-    private String[][]  mineFieldMap;
+    private String[][] mineFieldMap;
     private int N;
     private int M;
 
     public void setMineField(String mineField) throws IllegalArgumentException {
-        if(!validateMineField(mineField)) throw new IllegalArgumentException();
+        if (!validateMineField(mineField)) throw new IllegalArgumentException();
         String[] rowsCount = mineField.split("\\\n");
         M = rowsCount.length;
         N = rowsCount[0].length();
-        mineFieldMap = new String[M] [N];
-        for(int i = 0; i<rowsCount.length; i++){
+        mineFieldMap = new String[M][N];
+        for (int i = 0; i < rowsCount.length; i++) {
             String[] singleSign = rowsCount[i].split("");
-            for(int j = 0; j<N; j++) {
+            for (int j = 0; j < N; j++) {
                 mineFieldMap[i][j] = singleSign[j];
             }
         }
     }
 
-    private boolean validateMineField(String mineField){
+    private boolean validateMineField(String mineField) {
         String[] rows = mineField.split("\\n");
         int firstRowLength = rows[0].length();
         for (int i = 1; i < rows.length; i++) {
-            if(rows[i].length() != firstRowLength){
+            if (rows[i].length() != firstRowLength) {
                 return false;
             }
         }
-        if(!mineField.matches("[\\.\\*\\n]*")){
+        if (!mineField.matches("[\\.\\*\\n]*")) {
             return false;
-        }
-        else {
+        } else {
             return true;
         }
+    }
+
+    public String getHintField() throws IllegalStateException {
+        int[] [] hintField = new int[M] [N];
+        for( int i = 0; i<M; i++){
+            for( int j = 0; i<N; i++) {
+                if(mineFieldMap[i] [j].equals("*")){
+                    hintField[i] [j] = 0;
+                    adjustAdjacent(hintField,i,j);
+                }
+            }
+        }
+        return null;
+    }
+
+    private void adjustAdjacent(int[][] hintField, int i, int j){
+
     }
 
     public String[][] getMineFieldMap() {
         return mineFieldMap;
     }
 
-    public String getHintField() throws IllegalStateException {
-        return null;
-    }
+
 }
